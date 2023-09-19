85af87fa4d5074e7272b23378cecb1ee9bc3c67d
Dell-User <Dell-User@DESKTOP-4LJVQAT>
2023-09-19 03:40:43 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Vehicle_static_attribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicles vehicle1 = new Vehicles("Madza", "White", 4);
+            Vehicles vehicle2 = new Vehicles("suzuki", "Yellow", 4);
+
+            Console.WriteLine(vehicle1.Model);
+            Console.WriteLine(Vehicles.vehicleCount);
+
+            //freeeze
+            Console.Read();
+
+        }
+    }
+}
---
+++ Vehicles.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.Vehicle_static_attribute
+{
+    class Vehicles
+    {
+        public string Model;
+        public string color;
+        public int wheels;
+
+        public static int vehicleCount; // declared a static attribute
+
+        public Vehicles(string aModel, string aColor, int aWheels)
+        {
+            Model = aModel;
+            color = aColor;
+            wheels = aWheels;
+
+            vehicleCount++;
+
+
+        }
+    }
+}
